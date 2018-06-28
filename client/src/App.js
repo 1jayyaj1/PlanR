03e506ea1f31c3dc94898bb123988303126c6621
Jay Abi-Saad <jayabisaad@gmail.com>
2018-06-28 14:59:47 -0400

    Partial calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
 class App extends Component {
+  constructor() {
+    super();
 
-  componentDidMount() {
-    fetch('/users');
+    this.state = {
+      events: []
+    }
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="calendar" style={{height: '700px', width: '75%', padding: '5%'}}>
+        <BigCalendar
+            selectable={true}
+           events={[{
+              'title': 'Woohoo',
+              'allDay': false,
+              'start': moment().toDate(),
+              'end': moment().add(4, "hours").toDate()
+            },
+            {
+              'title': 'Ok',
+              'allDay': false,
+              'start': moment().add(1, "day").toDate(),
+              'end': moment().add(26, "hours").toDate()
+            }
+            ]}
+            startAccessor='start'
+            endAccessor='end'
+        />
       </div>
     );
   }
