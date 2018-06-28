feb85bb14b8b32342a8c8f65cf086ca6f890c330
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-06-28 10:55:18 -0400

    Connected backend with react

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('/users');
+  }
+
   render() {
     return (
       <div className="App">
