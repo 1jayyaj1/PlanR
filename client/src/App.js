6f5f3e7dbcf42565ca961081f3ce9ffe83320621
Luya <lorraineding0419@gmail.com>
2018-07-20 11:57:26 -0400

    Add Feedback

--- client/src/App.js
+++ client/src/App.js
@@ -528,6 +528,42 @@ class App extends Component {
           </div>
         </div>
 
+        {/*<---------------------FEEDBACK--------------------->*/}
+        <div class="contact section-invert py-4">
+          <h3 class="section-title text-center m-5">Contact Us</h3>
+          <div class="container py-4">
+            <div class="row justify-content-md-center px-4">
+              <div class="contact-form col-sm-12 col-md-10 col-lg-7 p-4 mb-4 card">
+                <form>
+                  <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                      <div class="form-group">
+                        <label for="contactFormFullName">Full Name</label>
+                        <input type="email" class="form-control" id="contactFormFullName" placeholder="Enter your full name"></input>
+                      </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                      <div class="form-group">
+                        <label for="contactFormEmail">Email address</label>
+                        <input type="email" class="form-control" id="contactFormEmail" placeholder="Enter your email address"></input>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <div class="form-group">
+                          <label for="exampleInputMessage1">Message</label>
+                          <textarea id="exampleInputMessage1" class="form-control mb-4" rows="10" placeholder="Enter your message..." name="message"></textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <input class="btn btn-primary btn-pill d-flex ml-auto mr-auto" type="submit" value="Send Your Message"></input>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/*<----------------------- FOOTER ----------------------->*/}                 
         <footer>
           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
