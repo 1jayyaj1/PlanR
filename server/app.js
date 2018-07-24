863bbd39aecc42210630e6c4bc7ffddef7cc5298
Luya <lorraineding0419@gmail.com>
2018-07-24 17:08:16 -0400

    Nodemailer set up and test
    with email : umba.testing@gmail.com

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventsRouter = require('./routes/events');
+var feedbackRouter = require('./routes/feedback');
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
+app.use('/feedback', feedbackRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
