6c339fe38b696116087b7364baf6270efe2afb8f
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-08-07 09:39:12 -0400

    Event re-modelling: PUT for all events at once

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,20 +1,6 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var router = express.Router();
 let Event =  require('../models/event');
-
-let types = ["Weekly", "Biweekly", "Triweekly", "Monthly"];
-let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-
-function remove(array, element) {
-    const index = array.indexOf(element);
-    
-    if (index !== -1) {
-        array.splice(index, 1);
-    }
-
-    console.log(array);
-}
 
 // for debugging only. will be removed in the future for security reasons
 router.get('/', function(req, res, next) {
@@ -30,9 +16,9 @@ router.get('/', function(req, res, next) {
         })
 });
 
-router.get('/:eventId', function(req, res, next) {
+router.get('/:parentId', function(req, res) {
     try {
-        Event.find({"_id" : req.params.eventId})
+        Event.find({"_id" : req.params.parentId})
             .exec()
             .then(events => {
                 if (events.length == 0) {
@@ -50,7 +36,7 @@ router.get('/:eventId', function(req, res, next) {
     }
 });
 
-router.delete('/all/:parentId', function(req, res, next) {
+router.delete('/all/:parentId', function(req, res) {
     const id = req.params.parentId;
     try {
         Event.findByIdAndRemove(id)
@@ -72,7 +58,7 @@ router.delete('/all/:parentId', function(req, res, next) {
 });
 
 
-router.delete('/:eventId', function(req, res, next) {
+router.delete('/:eventId', function(req, res) {
     const id = req.params.eventId;
     try {
         var parentId = null;
@@ -122,11 +108,90 @@ router.delete('/:eventId', function(req, res, next) {
     }
 });
 
-router.put('/:eventId', function(req, res, next) {
+router.put('/all/:parentId', function(req, res) {
+    const body = req.body;
+    const id = req.params.parentId;
+    try {
+        var newList = null;
+        var parentId = null;
+        Event.find()
+        .exec()
+        .then(events => {
+            if (events.length == 0) {
+                return res.sendStatus(404);
+            } else {
+                var found = null;
+                events.map(x => {
+                    if (x._id == id) {
+                        found = true;
+                        x.data.forEach(y => {
+                            if (body.calendarInfo) {
+                                if (body.calendarInfo.title) {
+                                    y.calendarInfo.allDay = body.calendarInfo.allDay;
+                                    if (/^[a-zA-Z- ]+$/.test(body.calendarInfo.title)) {
+                                        y.calendarInfo.title = body.calendarInfo.title;
+                                    } else {
+                                        return res.sendStatus(404);
+                                    }
+                                    var startDate = new Date(body.calendarInfo.start);
+                                    var endDate = new Date(body.calendarInfo.end);
+                                    if (startDate <= endDate) {
+                                        y.calendarInfo.start = startDate;
+                                        y.calendarInfo.end = endDate;
+                                    } else {
+                                        return res.sendStatus(400);
+                                    }
+                                }
+                            }
+                            if (body.capacity) {
+                                y.capacity = body.capacity;
+                            }
+                            if (body.location) {
+                                y.location = body.location;
+                            }
+                            if (body.description) {
+                                y.description = body.description;
+                            }
+                            if (body.activationDay) {
+                                y.activationDay = new Date(body.activationDay);
+                            }
+                        });
+                    }
+
+                    if (found) {
+                        newList = x.data;
+                        parentId = x._id;
+                    }
+                });
+                
+                Event.findByIdAndUpdate(parentId, {data: newList}, {new: true})
+                .then(event => {
+                    if (!event) {
+                        return res.status(404).send("Event not found with id " + id);
+                    }
+                    return res.send(event);
+                })
+                .catch(err => {
+                    console.log(err);
+                    if (err.code === 11000) {
+                        return res.status(404).send("Event not found with id " + id);
+                    }
+                    return res.sendStatus(500);
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            return res.sendStatus(404);
+        })
+    } catch {
+        return res.sendStatus(500);
+    }
+});
+
+router.put('/:eventId', function(req, res) {
     const body = req.body;
     const id = req.params.eventId;
-    var updateField = {};
-    updateField.calendarInfo = {};
     try {
         var parentId = null;
         var newList = null;
@@ -206,7 +271,7 @@ router.put('/:eventId', function(req, res, next) {
     }
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
     const body = req.body;
     try {
         var filtered = 
